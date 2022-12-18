5db52849ccf3fbe8b79cfd3d46a15613aab54991
aymaneBarri <ay.barri@edu.umi.ac.ma>
2022-12-18 11:38:56 +0100

    login

--- src/main/java/com/example/docapp/Application.java
+++ src/main/java/com/example/docapp/Application.java
@@ -2,6 +2,7 @@ package com.example.docapp;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,12 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("Log in");
+        stage.getIcons().add(new Image(Application.class.getResourceAsStream("images/icon.png")));
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setMaximized(false);
         stage.show();
     }
 
--- src/main/java/com/example/docapp/controllers/LoginController.java
+++ src/main/java/com/example/docapp/controllers/LoginController.java
@@ -1,14 +1,94 @@
 package com.example.docapp.controllers;
 
+import com.example.docapp.dataAccess.DBUtil;
+import com.example.docapp.models.Utilisateur;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TextField emailTextField;
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private Button loginButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLoginButtonClick() {
+//        DBUtil db = new DBUtil();
+//        Connection connection = db.getConnection();
+//
+////        String query = "Select * from utilisateur where email = '" + emailTextField.getText() + "' and password = '" + passwordTextField.getText() + "'";
+//
+//        try {
+//            PreparedStatement psLogin = connection.prepareStatement("SELECT * FROM utilisateur WHERE email = ? AND password = ?");
+//            psLogin.setString(1, emailTextField.getText());
+//            psLogin.setString(2, passwordTextField.getText());
+//            ResultSet queryOutput = psLogin.executeQuery();
+//
+//            if (queryOutput.isBeforeFirst()) {
+//                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+//                alert.setContentText("Connecté!");
+//                alert.show();
+//            } else {
+//                Alert alert = new Alert(Alert.AlertType.ERROR);
+//                alert.setContentText("Utilisateur non existant!");
+//                alert.show();
+//            }
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+//        String query = "Select * from utilisateur where email = '" + emailTextField.getText() + "' and password = '" + passwordTextField.getText() + "'";
+
+                try {
+                    Connection connection = DBUtil.getConnection();
+
+                    PreparedStatement psLogin = connection.prepareStatement("SELECT * FROM utilisateur WHERE email = ? AND password = ?");
+                    psLogin.setString(1, emailTextField.getText());
+                    psLogin.setString(2, passwordTextField.getText());
+                    ResultSet queryOutput = psLogin.executeQuery();
+
+                    if (queryOutput.isBeforeFirst()) {
+                        Utilisateur utilisateur = new Utilisateur();
+                        while(queryOutput.next()) {
+                            utilisateur = new Utilisateur(queryOutput.getInt(1), queryOutput.getString(2), queryOutput.getString(3), queryOutput.getString(4), queryOutput.getString(5), queryOutput.getString(6), queryOutput.getString(7));
+                        }
+
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                        alert.setContentText("Bienvenu " + utilisateur.getFirstName() + " " + utilisateur.getLastName());
+                        alert.show();
+                    } else {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("Utilisateur non existant!");
+                        alert.show();
+                    }
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    DBUtil.stopConnection();
+                }
+            }
+        });
     }
 }
