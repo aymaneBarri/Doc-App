43652e43227d78f7a43379ba13f6708d500d5b1f
aymaneBarri <ay.barri@edu.umi.ac.ma>
2023-01-13 19:18:56 +0100

    last fix

--- src/main/java/com/example/docapp/controllers/utilisateurs/UserPermissionController.java
+++ src/main/java/com/example/docapp/controllers/utilisateurs/UserPermissionController.java
@@ -14,6 +14,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
